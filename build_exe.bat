f6840beeec6f2d0eb87f5f4e915c17c7cb9c83c6
replittestacc1 <56701655-replittestacc1@users.noreply.replit.com>
2026-04-19 06:47:04 +0000

    Restored to 'a8b7df7935c8166cccce6d1db12746f0a1cf9382'
    Replit-Restored-To: a8b7df7935c8166cccce6d1db12746f0a1cf9382

--- build_exe.bat
+++ build_exe.bat
@@ -152,7 +152,6 @@ echo.
 set ADDDATA=
 if exist "splash_logo.png" set ADDDATA=--add-data "splash_logo.png;."
 if exist "Roblox2017.ttf" set ADDDATA=%ADDDATA% --add-data "Roblox2017.ttf;."
-if exist "Inter-Black.ttf" set ADDDATA=%ADDDATA% --add-data "Inter-Black.ttf;."
 
 if exist "icon.ico" (
     echo [*] Building with custom icon
